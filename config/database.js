49222bb7a23bf41b9e258d4becbb5f540992ea15
Colin Leung <colinmleung@gmail.com>
2015-08-09 00:55:44 -0700

    Made db connection dynamic for Heroku

--- config/database.js
+++ config/database.js
@@ -1,8 +1,14 @@
 // config/database.js
 module.exports = function(Sequelize) {
-	var db = new Sequelize('forum', 'postgres', '1qaz2wsx3edc', {
-		dialect: 'postgres',
-		host: 'localhost'
-	});
+	var db;
+	if (process.env.DATABASE_URL) {
+		db = new Sequelize(process.env.DATABASE_URL);
+	} else {
+		db = new Sequelize('forum', 'postgres', '1qaz2wsx3edc', {
+			dialect: 'postgres',
+			host: 'localhost'
+		});
+	}
+	
 	return db;
 };
